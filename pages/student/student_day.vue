0f061e5f600faee2ba48478d4a89383fec704e6a
王秀龙 <70106377@qq.com>
2020-01-30 15:01:23 +0800

    每日健康统计 安徽-unicloud-王秀龙

---
+++ pages/student/student_day.vue
@@ -0,0 +1,23 @@
+<!-- 每日健康统计 安徽-unicloud-王秀龙 -->
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
