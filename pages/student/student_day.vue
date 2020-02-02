dbe8e85605eb77961039a4caf3de06dbd6feca9d
王秀龙 <70106377@qq.com>
2020-02-02 15:21:25 +0800

    update pages/student/student_day.vue.

--- pages/student/student_day.vue
+++ pages/student/student_day.vue
@@ -1,22 +1,18 @@
 <!-- 每日健康统计 安徽-unicloud-王秀龙 -->
 <template>
 	<view>
-		<form @submit="formSubmit" @reset="formReset">
-		<view class="uni-title  uni-common-pl">学校</view>
+		<form @submit="formSubmit">
+		<view class="uni-title  uni-common-pl">学号</view>
 		<view class="uni-form-item">
-			<input class="uni-input" name="school" placeholder="请输入学校" />
-		</view>
-		<view class="uni-title  uni-common-pl">班级</view>
-		<view class="uni-form-item uni-column">
-			<input class="uni-input" name="class" placeholder="请输入班级" />
+			<input class="uni-input" v-model="student.stu_num" name="stu_num" placeholder="请输入学号" />
 		</view>
 		<view class="uni-title  uni-common-pl">姓名</view>
 		<view class="uni-form-item uni-column">
-			<input class="uni-input" name="name" placeholder="请输入姓名" />
+			<input class="uni-input" v-model="student.stu_name" name="stu_name" placeholder="请输入姓名" />
 		</view>
 		<view class="uni-title  uni-common-pl">有无接触湖北/武汉人员</view>
 		<view class="uni-list">
-			<radio-group name="contact_like_virus">
+			<radio-group name="contact_virus">
 				<label class="uni-list-cell uni-list-cell-pd">
 					<view>
 						<radio value="1" />
@@ -33,7 +29,7 @@
 		</view>
 		<view class="uni-title  uni-common-pl">有无疑似症状</view>
 		<view class="uni-list">
-			<radio-group name="Suspected_symptoms">
+			<radio-group name="have_symptom">
 				<label class="uni-list-cell uni-list-cell-pd">
 					<view>
 						<radio value="1" />
@@ -48,9 +44,39 @@
 				</label>
 			</radio-group>
 		</view>
-		<view class="uni-title  uni-common-pl">当前所在省市</view>
-		<view class="uni-form-item" @tap="openAddres">
-			<input class="uni-input" v-model="pickerText" disabled="true" name="check_in_address" placeholder="请选择省市" />
+		<view class="uni-title  uni-common-pl">当前位置</view>
+		<view class="uni-list">
+			<radio-group name="have_symptom">
+				<label class="uni-list-cell uni-list-cell-pd">
+					<view>
+						<radio value="0" />
+					</view>
+					<view class="k_left">本地</view>
+				</label>
+				<label class="uni-list-cell uni-list-cell-pd">
+					<view>
+						<radio value="1" />
+					</view>
+					<view class="k_left">外地（除湖北）</view>
+				</label>
+				<label class="uni-list-cell uni-list-cell-pd">
+					<view>
+						<radio value="2" />
+					</view>
+					<view class="k_left">外地（湖北）</view>
+				</label>
+			</radio-group>
+		</view>
+		<view class="uni-title  uni-common-pl">当前健康状况</view>
+		<view class="uni-list">
+			<checkbox-group @change="checkboxChange" name="health">
+				<label class="uni-list-cell uni-list-cell-pd" v-for="item in items" :key="item.value">
+					<view>
+						<checkbox :value="item.value" :checked="item.checked" />
+					</view>
+					<view>{{item.name}}</view>
+				</label>
+			</checkbox-group>
 		</view>
 		<view class="uni-title  uni-common-pl">今日体温</view>
 		<view class="uni-form-item uni-column">
@@ -60,7 +86,6 @@
 			<button form-type="submit" type="primary">提交</button>
 		</view>
 		</form>
-		<simple-address ref="simpleAddress" :pickerValueDefault="cityPickerValueDefault" @onConfirm="onConfirm" themeColor='#007AFF'></simple-address>
 	</view>
 </template>
 
@@ -68,19 +93,105 @@
 	export default {
 		data() {
 			return {
-				cityPickerValueDefault: [0, 0, 1],
-				pickerText: ''
+				checked:false,
+				checkeds:false,
+				student:{
+					stu_num:"",
+					stu_name:""
+				},
+				items: [{
+						value: '良好',
+						name: '良好'
+					},
+					{
+						value: '咳嗽',
+						name: '咳嗽'
+					},
+					{
+						value: '乏力',
+						name: '乏力'
+					},
+					{
+						value: '发热',
+						name: '发热'
+					}
+				]
 			}
 		},
+		onLoad:function(){
+			uni.showLoading({
+				title: '加载中...'
+			})
+			this.$cloud.callFunction({
+				name: 'getStudent',
+				data: {
+					stu_id: uni.getStorageSync('stu_id')
+				},
+			}).then((res) => {
+				this.student = res.result.data[0];
+				console.log(this.student);
+				uni.hideLoading()
+			}).catch((err) => {
+				console.log(err);
+				uni.hideLoading()
+				uni.showModal({
+					content: '加载失败，' + err.message,
+					showCancel: false
+				})
+			})
+		},
 		methods: {
-			openAddres() {
-				this.$refs.simpleAddress.open();
-			},
-			onConfirm(e) {
-				this.pickerText = e.label;
-			},
 			formSubmit: function(e) {
-				console.log(e)
+				var data ={
+					stu_id:this.student._id,
+					class_id:this.student.class_id,
+					create_time:Date.now()
+				}
+				var formData = e.detail.value, data;
+				formData.health = formData.health.join();
+				var submitData = Object.assign(formData, data);
+				
+				this.$cloud.callFunction({
+				  name: 'studentDay',
+				  data: submitData
+				}).then((res) => {
+				  uni.hideLoading()
+				  uni.showModal({
+					title: '提示',
+				    content: `提交成功`,
+				    showCancel: false,
+					success: function (res) {
+						if (res.confirm) {
+							console.log('用户点击确定');
+						} else if (res.cancel) {
+							console.log('用户点击取消');
+						}
+					}
+				  })
+				}).catch((err) => { 
+				  uni.hideLoading()
+				  uni.showModal({
+				    content: `提交数据失败，错误信息为：${err.message}`,
+				    showCancel: false
+				  })
+				  console.error(err)
+				})
+				console.log(submitData)
+			},
+			checkboxChange: function (e) {
+				var items = this.items,
+					values = e.detail.value;
+				for (var i = 0, lenI = items.length; i < lenI; ++i) {
+					const item = items[i]
+					if(values.indexOf(item.value) >= 0){
+						this.$set(item,'checked',true)
+					}else{
+						this.$set(item,'checked',false)
+					}
+					if(values.indexOf(items[0].value) >= 0){
+						this.$set(item,'checked',false)
+					}
+				}
 			}
 		}
 	}
@@ -93,7 +204,10 @@
 	.k_left {
 		margin-left: 16rpx;
 	}
+	
+	/* #ifdef H5 */
 	.K_botton {
 		margin-bottom: 100rpx;
 	}
+	/* #endif */
 </style>
