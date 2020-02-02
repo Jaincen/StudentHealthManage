1734817d2cfa9e7ba18202704bde50d2410e9b33
王秀龙 <70106377@qq.com>
2020-02-02 17:19:54 +0800

    根据学生id，查询学生信息

--- cloudfunctions-aliyun/getStudent/index.js
+++ cloudfunctions-aliyun/getStudent/index.js
@@ -1,5 +1,6 @@
 'use strict';
 const db = uniCloud.database()
+
 exports.main = async (event, context) => {
  const collection = db.collection('students')
  const res = await collection.doc(event.stu_id).get()
