60dc5cb35aba83899a9735f1c032e4a7d042e6cd
秦少卫 <nihaojob@163.com>
2020-02-02 15:21:31 +0800

    去掉spaceId

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 Vue.prototype.$cloud = uniCloud.init({
 	provider: 'aliyun',
-	spaceId: 'de6459c3-4d47-4544-ac63-2ebd61b5af59',
-	clientSecret: '6IQkNoqXMyA9ayTLWD3zeA=='
+	spaceId: '',
+	clientSecret: ''
 })
 const app = new Vue({
     ...App
