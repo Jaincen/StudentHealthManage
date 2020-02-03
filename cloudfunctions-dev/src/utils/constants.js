8f0413d9b900129f379629d1b70fcdba6c8b7a9f
秦少卫 <nihaojob@163.com>
2020-02-03 21:47:58 +0800

    测试按钮去除

--- cloudfunctions-dev/src/utils/constants.js
+++ cloudfunctions-dev/src/utils/constants.js
@@ -1,6 +1,6 @@
 const loginConfig = {
-	AppId: '', //微信小程序AppId
-	AppSecret: '' //微信小程序AppSecret
+	AppId: 'wxc6a6390e3dbe372f', //微信小程序AppId
+	AppSecret: '46ffc2c5373afa7113f7cdc5dfbb0416' //微信小程序AppSecret
 }
 
 const passSecret = '' //用于用户数据库密码加密的密钥，使用一个比较长的随机字符串即可
