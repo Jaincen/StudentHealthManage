1c06def19c0e3b416c685de72b8d93b0028f6288
秦少卫 <nihaojob@163.com>
2020-02-03 21:44:42 +0800

    用户类型修改

---
+++ cloudfunctions-dev/src/utils/getSchoolName.js
@@ -0,0 +1,11 @@
+const db = uniCloud.database()
+async function getSchoolName(token) {
+	try{
+		const userInDB = await db.collection('admin_users').get()
+		return userInDB[0].school
+	}catch(e){
+		return ''
+	}
+}
+
+export default getSchoolName
---
+++ cloudfunctions-dev/src/utils/userTypeConfig.js
@@ -0,0 +1,16 @@
+function userTypeConfig(userType) {
+	// 根据tab类型匹配对应用户表
+	// 0：老师
+	// 1：学生
+	// 2：家长
+	// 3：管理员
+	const userOptionDB = {
+		0:'teachers',
+		1:'students',
+		2:'parents',
+		3:'admin_users',
+	}
+	return userOptionDB[userType];
+}
+
+export default userTypeConfig
